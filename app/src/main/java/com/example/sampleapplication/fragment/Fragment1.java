85bd74976e995ab7b35b2ab88ed741025bc02cc7
panharith <huot.panharith.hs@moeys.gov.kh>
2021-08-05 06:32:33 +0700

    EditText Sample

--- app/src/main/java/com/example/sampleapplication/fragment/Fragment1.java
+++ app/src/main/java/com/example/sampleapplication/fragment/Fragment1.java
@@ -5,7 +5,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import androidx.annotation.Nullable;
@@ -20,19 +19,7 @@ public class Fragment1 extends BaseFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment1,container,false);
-        mButton = (Button)view.findViewById(R.id.button_submit);
-        mToggleButton1 = (ToggleButton)view.findViewById(R.id.toggleButton1);
-        mToggleButton2 = (ToggleButton)view.findViewById(R.id.toggleButton2);
-        mButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                StringBuilder result = new StringBuilder();
-                result.append("ToggleButton1:").append(mToggleButton1.getText());
-                result.append("\nToggleButton2:").append(mToggleButton2.getText());
 
-                Toast.makeText(getActivity(), result.toString(), Toast.LENGTH_SHORT).show();
-            }
-        });
         return view;
     }
 }
--- app/src/main/java/com/example/sampleapplication/fragment/HomeFragment.java
+++ app/src/main/java/com/example/sampleapplication/fragment/HomeFragment.java
@@ -5,27 +5,37 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
 import com.example.sampleapplication.R;
-import com.example.sampleapplication.activity.BaseActivity;
 
 public class HomeFragment extends BaseFragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
-        Button myButton = (Button) view.findViewById(R.id.button_home);
-        myButton.setOnClickListener(new View.OnClickListener() {
+        View view = inflater.inflate(R.layout.fragment_edittext, container, false);
+        EditText editText = view.findViewById(R.id.editText);
+        Button button = view.findViewById(R.id.button_show);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                BaseFragment fragment = new Fragment1();
-                if (fragment != null) {
-                    ((BaseActivity) getActivity()).pushFragment(fragment);
-                }
+                Toast.makeText(getActivity(), editText.getText(), Toast.LENGTH_LONG).show();
             }
         });
+
+//        Button myButton = (Button) view.findViewById(R.id.button_home);
+//        myButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                BaseFragment fragment = new Fragment1();
+//                if (fragment != null) {
+//                    ((BaseActivity) getActivity()).pushFragment(fragment);
+//                }
+//            }
+//        });
         return view;
     }
 }
