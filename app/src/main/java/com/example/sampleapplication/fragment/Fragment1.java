14e1df0c3d42113d2a476e98673a3ccd45533f84
panharith <huot.panharith.hs@moeys.gov.kh>
2021-08-29 16:17:12 +0700

    update 29/8/2021

--- app/src/main/java/com/example/sampleapplication/fragment/Fragment1.java
+++ app/src/main/java/com/example/sampleapplication/fragment/Fragment1.java
@@ -4,16 +4,28 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 
 import com.example.sampleapplication.R;
+import com.example.sampleapplication.activity.BaseActivity;
 
-public class Fragment1 extends BaseFragment{
+public class Fragment1 extends BaseFragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment1,container,false);
+        View view = inflater.inflate(R.layout.fragment1, container, false);
+        ImageView imageView = (ImageView) view.findViewById(R.id.imv_fragment1);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BaseFragment fragment = new Fragment2();
+                if (null != fragment) {
+                    ((BaseActivity) getActivity()).pushFragment(fragment);
+                }
+            }
+        });
         return view;
     }
 }
